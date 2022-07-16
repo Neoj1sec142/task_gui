0b24248bf492720d7c9853e9acca24f70613e908
Mark Harmon <neothej1sec@gmail.com>
2022-07-15 20:57:37 -0500

    finished basic set up of task magaement app

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ win = Tk()
 win.geometry("800x470")
 win.title("Task Management CRUD App")
 
+tId = Label(win, text="ID:", font=("Serif", 12))
+tId.place(x=20, y=120) 
 tPrio = Label(win, text="Priority:", font=("Serif", 12))
 tPrio.place(x=20, y=30)
 tTit = Label(win, text="Title:", font=("Serif", 12))
@@ -14,6 +16,8 @@ tTit.place(x=20, y=60)
 tLoc = Label(win, text="Location:", font=("Serif", 12))
 tLoc.place(x=20, y=90)
 
+entId = Entry(win)
+entId.place(x=170, y=120)
 entPrio = Entry(win)
 entPrio.place(x=170, y=30)
 entTit = Entry(win)
@@ -22,26 +26,29 @@ entLoc = Entry(win)
 entLoc.place(x=170, y=90)
 
 def insert():
+    iD = entId.get()
     p = entPrio.get()
     t = entTit.get()
     l = entLoc.get()
-    if(p=="" or t=="" or l==""):
+    if(iD=="" or p=="" or t=="" or l==""):
         mb.showwarning("Cannot Insert", "All Fields Required")
     else:
         db = msc.connect(host='localhost', user='root', passwd=os.environ.get('PASSWD'), database='task', auth_plugin='mysql_native_password')
         cur = db.cursor()
-        cur.execute("INSERT INTO task.taskDet (tPrio,tTit,tLoc) VALUES (%s,%s,%s)", (p,t,l))
+        cur.execute("INSERT INTO task.taskDet (tId,tPrio,tTit,tLoc) VALUES (%s,%s,%s,%s)", (iD,p,t,l))
         db.commit()
+        entId.delete(0, "end")
         entPrio.delete(0, "end")
         entTit.delete(0, "end")
         entLoc.delete(0, "end")
-        # show()
+        show()
         mb.showinfo("Insert status", "Data Inserted Successfully")
         db.close()
 inBtn = Button(win, text="Insert", font=("Sans", 12), bg="white", command=insert)
 inBtn.place(x=20,y=160)
 
 def update():
+    iD = entId.get()
     p = entPrio.get()
     t = entTit.get()
     l = entLoc.get()
@@ -50,51 +57,72 @@ def update():
     else:
         db = msc.connect(host='localhost', user='root', passwd=os.environ.get('PASSWD'), database='task', auth_plugin='mysql_native_password')
         cur = db.cursor()
-        cur.execute("UPDATE taskDet set tPrio=%s,tLoc=%s where tTit=%s", (p,l,t))
+        cur.execute("UPDATE taskDet set tPrio=%s,tLoc=%s,tTit=%s where tId=%s", (p,l,t, iD))
         db.commit()
+        entId.delete(0, "end")
         entPrio.delete(0, "end")
         entTit.delete(0, "end")
         entLoc.delete(0, "end")
-        #show()
+        show()
         mb.showinfo("Update Status:", "Data Updated Successfully")
         db.close()
-upBtn = Button(win, text="Update", fornt=("Sans",12), bg="white", command=update)
+upBtn = Button(win, text="Update", font=("Sans",12), bg="white", command=update)
 upBtn.place(x=80,y=160)
 
 def getTasks():
-    if(entTit.get()==""):
-        mb.showwarning("Get Status", "Please enter the title of the task you wish to get.")
+    if(entId.get()==""):
+        mb.showwarning("Get Status", "Please enter the ID of the task you wish to get.")
     else:
         db = msc.connect(host='localhost', user='root', passwd=os.environ.get('PASSWD'), database='task', auth_plugin='mysql_native_password')
         cur = db.cursor()
-        cur.execute("SELECT * FROM taskDet where tTit="+entTit.get()+"")
+        cur.execute("SELECT * FROM taskDet where tId="+entId.get()+"")
         rows = cur.fetchall()
         for row in rows:
-            entPrio.insert(0, row[1])
+            entPrio.insert(0, row[0])
+            entTit.insert(0, row[1])
             entLoc.insert(0, row[2])
         db.close()
 getBtn = Button(win, text="Get", font=("Sans", 12), bg="white", command=getTasks)
 getBtn.place(x=150,y=160)
 
 def delete():
-    if(entTit.get()==""):
-        mb.showwarning("Please provide the title of the task you wish to delete.")
+    if(entId.get()==""):
+        mb.showwarning("Please provide the ID of the task you wish to delete.")
     else:
         db = msc.connect(host='localhost', user='root', passwd=os.environ.get('PASSWD'), database='task', auth_plugin='mysql_native_password')
         cur = db.cursor()
-        cur.execute("DELETE FROM taskDet where tTit="+entTit.get()+"")
+        cur.execute("DELETE FROM taskDet where tId="+entId.get()+"")
         db.commit()
+        entId.delete(0, "end")
         entPrio.delete(0, "end")
         entTit.delete(0, "end")
         entLoc.delete(0, "end")
-        # show()
+        show()
         mb.showinfo("Delete Status", "Data Deleted Successfully")
         db.close()
 dltBtn = Button(win, text="Delete", font=("Sans", 12), bg="white", command=delete)
 dltBtn.place(x=210,y=160)
 
+def reset():
+    entId.delete(0, "end")
+    entPrio.delete(0, "end")
+    entTit.delete(0, "end")
+    entLoc.delete(0, "end")
+rstBtn = Button(win, text="Reset", font=("Sans", 12), bg="white", command=reset)
+rstBtn.place(x=20,y=210)
+
+def show():
+    db = msc.connect(host='localhost', user='root', passwd=os.environ.get('PASSWD'), database='task', auth_plugin='mysql_native_password')
+    cur = db.cursor()
+    cur.execute("SELECT * FROM taskDet")
+    rows = cur.fetchall()
+    showData.delete(0, showData.size())
+    for row in rows:
+        addData = str(row[0])+' '+str(row[1])+' '+row[2]+' '+row[3]
+        showData.insert(showData.size()+1, addData)
+    db.close()
 
 showData = Listbox(win)
 showData.place(x=330, y=30)
-# show()
+show()
 win.mainloop()
